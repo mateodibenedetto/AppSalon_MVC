22bd3e5fe49e9a700682e1eeecebb96f4c31d0d5
Mateo <mateodibenedetto01@gmail.com>
2022-04-11 21:33:46 -0300

    db2

--- includes/database.php
+++ includes/database.php
@@ -3,10 +3,16 @@
 // Variables de Entorno
 
 $db = mysqli_connect(
-    $_ENV['DB_HOST'],
-    $_ENV['DB_USER'],
-    $_ENV['DB_PASS'], // ?? '' 
-    $_ENV['DB_BD']
+    // Local Host
+    // $_ENV['DB_HOST'],
+    // $_ENV['DB_USER'],
+    // $_ENV['DB_PASS'], // ?? '' 
+    // $_ENV['DB_BD']
+
+    $_ENV['DB_HOST_P'],
+    $_ENV['DB_USER_P'],
+    $_ENV['DB_PASS_P'],
+    $_ENV['DB_BD_P']
 );
 
 
